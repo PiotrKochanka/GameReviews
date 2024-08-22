cdbf7489c6af9d694782b280420e2c4e7b476bf4
unknown <kochankap@gmail.com>
2024-08-22 18:09:08 +0200

    Adding a new project with front end, and back end

---
+++ game-reviews/src/App.js
@@ -0,0 +1,13 @@
+import { Button, Container, Row, Col } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ecommerce project</h1>
+      <Button>bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
