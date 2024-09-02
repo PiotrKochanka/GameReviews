797fb9ac375cadf4389203633ee3caed50137243
unknown <kochankap@gmail.com>
2024-09-02 18:18:32 +0200

    new database

--- game-reviews-backend/routes/web.php
+++ game-reviews-backend/routes/web.php
@@ -1,24 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RSSFeedController;
-use App\Http\Controllers\RSSFeedController2;
-
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
 
+// Nowa trasa do pobierania RSS
 Route::get('/rss-feed', [RSSFeedController::class, 'getFeed']);
-Route::get('/rss-feed2', [RSSFeedController2::class, 'getFeed']);
+
+// Dołącz inne pliki z trasami, jeśli istnieją
+require __DIR__.'/auth.php';
