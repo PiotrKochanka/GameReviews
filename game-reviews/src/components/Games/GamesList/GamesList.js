63afd0d0079600f0071d40f913ef1998bbc2eb4b
unknown <kochankap@gmail.com>
2024-11-12 20:55:44 +0100

    Games searchbar

--- game-reviews/src/components/Games/GamesList/GamesList.js
+++ game-reviews/src/components/Games/GamesList/GamesList.js
@@ -3,60 +3,81 @@ import React, { useState } from 'react';
 
 function GamesList({ games }) {
     const [filter, setFilter] = useState('all');
+    const [searchTerm, setSearchTerm] = useState(''); // Nowy stan dla wyszukiwania
     const baseUrl = 'http://127.0.0.1:8000/storage/';
 
     const handleFilterChange = (e) => {
         setFilter(e.target.value);
     };
 
-    // Filtruj gry na podstawie wybranego filtra
+    const handleSearchChange = (e) => {
+        setSearchTerm(e.target.value.toLowerCase()); // Przechowujemy małe litery, by uprościć porównanie
+    };
+
+    // Filtruj gry na podstawie wybranego filtra oraz wyszukiwanego terminu
     const filteredGames = games.filter((item) => {
-        if (filter === 'all') return true;
-        if (filter === 'best') return item.score > 7;
-        if (filter === 'worst') return item.score <= 4.5;
-        return false;
+        const matchesFilter =
+            filter === 'all' ||
+            (filter === 'best' && item.score > 7) ||
+            (filter === 'worst' && item.score <= 4.5);
+
+        const matchesSearch = item.title.toLowerCase().includes(searchTerm);
+
+        return matchesFilter && matchesSearch;
     });
 
     return (
         <div className={`${styles.games_container} container`}>
-            <div className={`${styles.games_filter} ${styles.games_column1}`}>
-                <h2 className={`${styles.title} start-title container`}>Filtry</h2>
-                <div className={`${styles.filters}`}>
-                    <ul>
-                        <li>
-                            <input
-                                type="radio"
-                                name="filter"
-                                value="all"
-                                id="all"
-                                checked={filter === 'all'}
-                                onChange={handleFilterChange}
-                            />
-                            <label htmlFor="all">Wszystkie</label>
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="filter"
-                                value="best"
-                                id="best"
-                                checked={filter === 'best'}
-                                onChange={handleFilterChange}
-                            />
-                            <label htmlFor="best">Najwyżej oceniane</label>
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="filter"
-                                value="worst"
-                                id="worst"
-                                checked={filter === 'worst'}
-                                onChange={handleFilterChange}
-                            />
-                            <label htmlFor="worst">Najniżej oceniane</label>
-                        </li>
-                    </ul>
+            <div className={`${styles.games_filters} ${styles.games_column1}`}>
+                <div className={`${styles.games_filter}`}>
+                    <h2 className={`${styles.title} start-title container`}>Szukaj</h2>
+                    <input 
+                        type="text"
+                        placeholder='Wyszukaj grę'
+                        className={`${styles.searchbar}`}
+                        value={searchTerm}
+                        onChange={handleSearchChange} // Obsługa zmiany w polu wyszukiwania
+                    />
+                </div>
+                <div className={`${styles.games_filter}`}>
+                    <h2 className={`${styles.title} start-title container`}>Ocena</h2>
+                    <div className={`${styles.filters}`}>
+                        <ul>
+                            <li>
+                                <input
+                                    type="radio"
+                                    name="filter"
+                                    value="all"
+                                    id="all"
+                                    checked={filter === 'all'}
+                                    onChange={handleFilterChange}
+                                />
+                                <label htmlFor="all">Wszystkie</label>
+                            </li>
+                            <li>
+                                <input
+                                    type="radio"
+                                    name="filter"
+                                    value="best"
+                                    id="best"
+                                    checked={filter === 'best'}
+                                    onChange={handleFilterChange}
+                                />
+                                <label htmlFor="best">Najwyżej oceniane</label>
+                            </li>
+                            <li>
+                                <input
+                                    type="radio"
+                                    name="filter"
+                                    value="worst"
+                                    id="worst"
+                                    checked={filter === 'worst'}
+                                    onChange={handleFilterChange}
+                                />
+                                <label htmlFor="worst">Najniżej oceniane</label>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div className={`${styles.games_column2}`}>
